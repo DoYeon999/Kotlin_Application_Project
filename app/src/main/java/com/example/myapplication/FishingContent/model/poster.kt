0417fb0f45461ac604f0280521f31c8f8deb20de
ghwntksanfaldur <tmdqts55@naver.com>
2023-08-11 16:54:30 +0900

    포스터 수정 완료 . 2023 . 08 . 11

--- app/src/main/java/com/example/myapplication/FishingContent/model/poster.kt
+++ app/src/main/java/com/example/myapplication/FishingContent/model/poster.kt
@@ -1,6 +1,12 @@
 package com.example.myapplication.FishingContent.model
 
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
 data class Poster(
+    @SerializedName("title")
     val title: String,
-//    val date: String ,
-    val ps: String )
+    @SerializedName("date")
+    val date : String,
+    @SerializedName("ps")
+    val ps: String ) : Serializable
