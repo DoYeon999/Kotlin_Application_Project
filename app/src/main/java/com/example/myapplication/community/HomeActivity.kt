30465debe916948cac41021e86f01aabd5b8048c
chocozzang <jangjh_05@naver.com>
2023-09-06 16:52:49 +0900

    230906

--- app/src/main/java/com/example/myapplication/community/HomeActivity.kt
+++ app/src/main/java/com/example/myapplication/community/HomeActivity.kt
@@ -8,7 +8,6 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.myapplication.MainActivity
 import com.example.myapplication.R
